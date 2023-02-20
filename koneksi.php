a2b0ba4991e95cfb81b3f8b7c15e6c57ec60cd90
Nurfitrayani <nurfitrayani@kallabs.ac.id>
2023-02-20 07:15:25 -0800

    first commit

---
+++ koneksi.php
@@ -0,0 +1,8 @@
+<?php
+$Host = "localhost";
+$User= "root";
+$Pass= "";
+$database_name = "final";
+$database = mysql_select_db ("$database_name");
+$koneksi = mysql_connect("$Host, $User, $Pass");
+?>
